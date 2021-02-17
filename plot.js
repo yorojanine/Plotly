79d549bcef9a445a6cc4ae1d452a14d017bc24fc
yorojanine <j9yoro@gmail.com>
2021-02-16 23:36:11 -0800

    module

---
+++ plot.js
@@ -0,0 +1,38 @@
+var sortedCities = cityGrowths.sort((a,b) =>
+a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
+
+var topFiveCities = sortedCities.slice(0,5);
+
+var topFiveCityNames = topFiveCities.map(city => city.City);
+var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
+
+var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: { title: "City" },
+    yaxis: { title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
+
+  var topSevenCities = sortedCities.slice(0,7);
+
+var topSevenCityNames = topSevenCities.map(city => city.City);
+var topSevemCityGrowths = topSevenCities.map(city => parseInt(city.Increase_from_2016));
+
+var trace = {
+    x: topSevenCityNames,
+    y: topSevemCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Top 7 Rapidly Growing Cities",
+    xaxis: { title: "City" },
+    yaxis: { title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot-seven", data, layout);
